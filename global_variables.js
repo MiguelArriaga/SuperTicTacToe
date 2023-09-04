b8129a7e3ac47028dcc64f3cbe86569eb737acaa
Miguel Arriaga <miguelarriagaecunha@gmail.com>
2023-09-04 12:47:00 +0100

    Added test button to prefill

--- global_variables.js
+++ global_variables.js
@@ -1,3 +1,36 @@
 var player = "X";
 var last_cell = 0;
+function FillTest() {
+
+    play(document.getElementById(`board1_cell1`))
+    play(document.getElementById(`board1_cell2`))
+    play(document.getElementById(`board2_cell6`))
+    play(document.getElementById(`board6_cell9`))
+    play(document.getElementById(`board9_cell9`))
+    play(document.getElementById(`board9_cell7`))
+    play(document.getElementById(`board7_cell8`))
+    play(document.getElementById(`board8_cell9`))
+    play(document.getElementById(`board9_cell5`))
+    play(document.getElementById(`board5_cell7`))
+    play(document.getElementById(`board7_cell4`))
+    play(document.getElementById(`board4_cell5`))
+    play(document.getElementById(`board5_cell1`))
+    play(document.getElementById(`board1_cell3`))
+    play(document.getElementById(`board3_cell2`))
+    play(document.getElementById(`board2_cell1`))
+    play(document.getElementById(`board1_cell5`))
+
+    play(document.getElementById(`board5_cell6`))
+    play(document.getElementById(`board6_cell2`))
+    play(document.getElementById(`board2_cell5`))
+    play(document.getElementById(`board5_cell5`))
+    play(document.getElementById(`board5_cell3`))
+    play(document.getElementById(`board3_cell7`))
+    play(document.getElementById(`board7_cell1`))
+    play(document.getElementById(`board1_cell9`))
+    play(document.getElementById(`board9_cell2`))
+    play(document.getElementById(`board2_cell3`))
+    play(document.getElementById(`board3_cell9`))
+
+}
 
