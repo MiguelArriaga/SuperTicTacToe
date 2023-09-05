11739831adb4327fa81f44001ebe029cac70a3f1
Miguel Arriaga <miguelarriagaecunha@gmail.com>
2023-09-05 18:10:18 +0100

    Added explanation on how to add more AIs

--- AI/AI_random.js
+++ AI/AI_random.js
@@ -1,20 +1,58 @@
 
+/**
+ * 
+ * @param {Array[Array[String]]} boardList Is an Array[9], where each element correspons to a board, and can be:
+ *                                         (1) "X","O","Tie" - if that board already is closed
+ *                                         (2) Array[9] with the moves already played in that board ("X", "O", "")  
+ * @param {Integer} last_move_cell The cell that the previous player played. The `curr_player` needs to play
+ *                                 in the board with this number unless `last_move_cell==0` which means that
+ *                                 `curr_player` can play on any empty cell of the board. 
+ * @param {String} curr_player Can be "X" or "O" and indicates which player is making a move
+ * @returns [board,cell] represents the next move, where `board` is an integer 1-9 corresponding to the
+ *                       position in the master_board (i.e. which small board in the large grid) and `cell`
+ *                       is an integer 1-9 corresponding to the position on the selected `board` 
+ */
 function AI_random_bestMove(boardList, last_move_cell, curr_player) {
-    let available_cells = []
-    // console.log(last_move_cell)
-    // debugger;
-    for (let board of [1, 2, 3, 4, 5, 6, 7, 8, 9]) {
-        if (!MASTER_CLOSED_BOARD_OPTIONS.includes(boardList[board-1]) && (board == last_move_cell || last_move_cell == 0)) {
-        // if (!MASTER_CLOSED_BOARD_OPTIONS.includes(boardList[board - 1])) {
-            for (let cell of [1, 2, 3, 4, 5, 6, 7, 8, 9]) {
-                if (!["X", "O"].includes(boardList[board - 1][cell - 1])) {
-                    available_cells.push([board, cell])
-                }
-            }
-        }
-    }
-
+    // Get possible moves
+    let available_cells = AI_random_getPossibleMoves(boardList, last_move_cell)
     let rand_elem = Math.floor(Math.random() * available_cells.length);
     return available_cells[rand_elem]
 
+}
+
+function AI_random_getPossibleMoves(boardList, last_move_cell) {
+    let possible_moves = []
+    
+    // Check each small board
+    for (let board of [1, 2, 3, 4, 5, 6, 7, 8, 9]) {
+        // If board is compatible with last_move
+        if (board == last_move_cell || last_move_cell == 0) {
+            // Get empty cells of that board and add them to the list of possible moves
+            let empty_cells = AI_random_getEmptyCells(boardList[board - 1])
+            for (let empty_cell of empty_cells) {
+                possible_moves.push([board, empty_cell])
+            }
+        }
+    }
+    return possible_moves
+}
+
+
+
+function AI_random_getEmptyCells(smallBoard) {
+    let empty_cells = []
+
+    // Check if board is already closed, i.e. there are no empty cells
+    if (MASTER_CLOSED_BOARD_OPTIONS.includes(smallBoard)) {
+        return empty_cells
+    }
+
+    // For each cell, check if it empty and add to list of empty cells
+    for (let cell of [1, 2, 3, 4, 5, 6, 7, 8, 9]) {
+        if (smallBoard[cell - 1] == "") {
+            empty_cells.push(cell)
+        }
+    }
+
+    return empty_cells
 }
--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 This is a website to play Super Tic Tac Toe: https://en.wikipedia.org/wiki/Ultimate_tic-tac-toe
 
 The end goal is to allow other people to submit their own AIs and either play against them or pit two AIs against each other.
+
+To submit an AI follow these steps:
+ 1. add a file called "AI_your_ai_name.js" to the "AI" folder
+ 2. In you file, have a function called `AI_your_ai_name_bestMove()` that returns the best next move (see `AI_random_bestMove()` for documentation)
+ 3. In the "AI/AI_base.js" file, add your AI to the dictionary called `AIs` as such:
+    - `'your_ai_name': { 'Name': "Your AI Name", "Function": AI_your_ai_name_bestMove },`
